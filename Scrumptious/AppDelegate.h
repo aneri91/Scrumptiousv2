7f9adffeb2617de17b8dc275b9ad40901bf39b2b
Karan Patel <karanpatel@karans-macbook-pro.local>
2016-07-03 11:57:25 +0530

    Initial Commit

---
+++ Scrumptious/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Scrumptious
+//
+//  Created by Karan Patel on 03/07/16.
+//  Copyright © 2016 Karan Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Scrumptious/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Scrumptious
+//
+//  Created by Karan Patel on 03/07/16.
+//  Copyright © 2016 Karan Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
